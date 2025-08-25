35516bc6ec5dc76837c288255addc9c2b67d9d0c
Ashish Namdeo <ashishnamdev14@gmail.com>
2025-08-25 14:44:40 -0700

    Create floor-in-a-sorted-array.java

---
+++ floor-in-a-sorted-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findFloor(int[] arr, int x) {
+        // code here
+        int start =0;
+        int end = arr.length-1;
+        int floor = -1;
+        while(start <= end){
+            int mid = start +(end-start)/2;
+            if(arr[mid] > x){
+                end =mid-1;
+            }else if(arr[mid] <= x){
+                floor = mid;
+                start = mid+1;
+            }
+        }
+        return floor;
+    }
+}
