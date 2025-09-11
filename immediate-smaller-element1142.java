acf992bb38761496db054f950eda74dc5f06d50c
Ashish Namdeo <ashishnamdev14@gmail.com>
2025-09-10 23:19:01 -0700

    Create immediate-smaller-element1142.java

---
+++ immediate-smaller-element1142.java
@@ -0,0 +1,26 @@
+class Solution {
+    static ArrayList<Integer> nextSmallerEle(int[] arr) {
+        // code here
+        ArrayList<Integer> list = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        for(int i=arr.length-1;i>=0;i--){
+            if(stack.isEmpty()){
+                list.add(-1);
+            }else if(!stack.isEmpty() && arr[i] > stack.peek()){
+                list.add(stack.peek());
+            }else if(!stack.isEmpty() && arr[i] <= stack.peek()){
+                while(!stack.isEmpty() && arr[i] <= stack.peek()){
+                    stack.pop();
+                }
+                if(stack.isEmpty()){
+                    list.add(-1);
+                }else{
+                  list.add(stack.peek());
+                }
+            }
+            stack.push(arr[i]);
+        }
+        Collections.reverse(list);
+        return list;
+    }
+}
